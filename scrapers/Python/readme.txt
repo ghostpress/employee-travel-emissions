03cac54a2b115038ebe753cb59df4b576dd56b94
Lucia V <luciamvila@gmail.com>
2020-11-20 10:09:59 -0500

    added subdirectories and initial python code

---
+++ scrapers/Python/readme.txt
@@ -0,0 +1,10 @@
+This code is written in Python 3 for scraping data from websites.
+It contains a class, Scraper, that handles all of the browser navigation
+and getting content from the Selenium driver. It requires the selenium
+library for Python and the chromedriver by Google.
+
+The "functions.py" file contains all other functions used to control the
+main scraper object. All content scraped from the scraper should be
+saved in the "./data" directory.
+
+The "run.py" file runs the program to iteratively scrape a site.
---
+++ scrapers/Python/run.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Set the working directory
+import os
+import sys
+path = '/home/lucia/bu/sustainability/ccl/code/scraperToolkit/Python/'  # edit according to system
+
+os.chdir(path)
+# print(os.getcwd()) - print test passed 11/18/20
+
+# Import functions, settings, and data
+sys.path.insert(0, './code')
+from functions import*
+
+scraper = Scraper()
+scraper.openBrowser()
+
+# -----------------------------------------------------------------------------
+# Some common methods
+
+# Get the soup
+soup = scraper.getSoup()
+
+# Find all divs
+divs = soup.find_all('div')
+
+# Find a thing by a name
+div = soup.find('div', {'id' : 'idName'})
+div = soup.find('div', {'class' : 'className'})
+
