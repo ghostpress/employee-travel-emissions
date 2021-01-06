bc0cdae24a476db5d0f6fedc53ea016c98c23d65
Lucia V <luciamvila@gmail.com>
2021-01-06 12:15:12 -0500

    Extracted departure and arrival codes from dataframe into list, started code to count instances

--- clean/run.py
+++ clean/run.py
@@ -4,28 +4,22 @@ import numpy as np
 
 # Read csv file into DataFrame
 df_subset = pd.read_csv('concur_flights_working_data_subset.csv')
-# print(df_subset.to_string())
-test = df_subset.get('Departure Station Code')
-# print(test)
-# print(type(test))
-# print(test.get(2))
 
 # Extract departure and arrival codes for each trip into a dictionary to count them
 # Key is [departure, arrival] and value is count
 
 # First, get the codes in a list
-trips_list = np.empty([42, 2], dtype=str)  # initialize empty string list w/ 42 rows, 2 columns
 
-for index, row in df_subset.iterrows():
-    trips_list[(row, 0)] = row[7]   # departure station codes in column 7
-    trips_list[(row, 1)] = row[13]  # arrival station codes in column 13
-    print(trips_list)
+dep_list = df_subset['Departure Station Code'].values.tolist()  # extract departure code column into list
+arr_list = df_subset['Arrival Station Code'].values.tolist()    # extract arrival code column into list
 
-# for row in range(0, 42):
-    # trips_list[row][0] = (df_subset.get('Departure Station Code')).get(row)
-    # trips_list[row][1] = (df_subset.get('Arrival Station Code')).get(row)
-
-print(trips_list)
+trips_list = np.array((dep_list, arr_list))  # combine the two lists into one
+trips_list = trips_list.T  # transpose the list to get [[arr, dep]]
+print(trips_list)  # print test passed 1/6/21
 
 # Now count the instances of each departure-arrival pair
+
+count = np.zeros(42, dtype=int)  # initialize array for counting trips
+print(count)  # print test passed 1/6/21
+
 
