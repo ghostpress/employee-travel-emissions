3a36599351bad459e0a9cd957ba552acd0307592
Lucia V <luciamvila@gmail.com>
2021-01-20 11:14:26 -0500

    Reorganized files

--- analysis/run.py
+++ analysis/run.py
@@ -1,29 +1,25 @@
 # Import libraries and functions
 
 import pandas as pd
-from functions import *
+from code import functions
 import plotly.graph_objects as go
 
 # Read csv file into DataFrame
-df_subset = pd.read_csv('concur_flights_working_data_subset.csv')  # subset to test
+df_subset = pd.read_csv('data/concur_flights_working_data_subset.csv')  # subset to test
+df_full = pd.read_csv('data/cleaned_full.csv')                          # full data set, incl. emissions
 
-# ------- Functions for counting and displaying in a table the most frequent routes in the data -------
+# Using the functions, get the departure and arrival cities
 
-# Using the functions, get the codes into a single list
+dep_list = functions.extract_column(df_full, 'Departure City')
+arr_list = functions.extract_column(df_full, 'Arrival City')
 
-dep_list = airport_codes_from_df(df_subset, 'Departure Station Code')
-arr_list = airport_codes_from_df(df_subset, 'Arrival Station Code')
+trips = functions.combine_codes(dep_list, arr_list)  # Combine the two lists into one
 
-trips = combine_codes(dep_list, arr_list)
-
-# Now count the duplicate trips
-dups = count_duplicates(trips)
+# Now count the number of trips
+dups = functions.count_duplicates(trips)
 
 most_common = dups.most_common(5)  # Get the 5 most common routes
-# print(most_common)
-
-freq = dups.values()
-print(list(freq))
+freq = dups.values()               # Get the list of counts
 
 # Make a table
 table = go.Figure(data=[go.Table(
@@ -31,8 +27,8 @@ table = go.Figure(data=[go.Table(
                 line_color='black',
                 fill_color='white',
                 align='left'),
-    cells=dict(values=[[1, 2, 3, 4, 5],  # First column
-                       most_common,      # Second column
+    cells=dict(values=[[1, 2, 3, 4, 5],        # First column
+                       most_common,            # Second column
                        list(dups.values())],   # Third column
                line_color='black',
                fill_color='white',
@@ -40,6 +36,3 @@ table = go.Figure(data=[go.Table(
 ])
 
 print('Table correctly created.')
-table.update_layout(width=500, height=300)
-table.show
-
