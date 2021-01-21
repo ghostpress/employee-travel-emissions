0737dd38a64f8a9f41b37d20d4f9aa728fdb8572
Lucia V <luciamvila@gmail.com>
2021-01-21 11:48:45 -0500

    Finished cleaning code, added comments

--- clean/Python/code/functions.py
+++ clean/Python/code/functions.py
@@ -64,6 +64,67 @@ def drop_zero_distance(df, results_path):
             drop.append(i)
 
     df_copy = df_copy.drop(drop)
-    df_copy.to_csv(results_path)
+    df_copy.to_csv(results_path, index=False)
     print('Finished in ' + str((time.time() - start) / 60) + ' mins. ' + 'Please find the remaining data in '
+          + results_path + '.')
+
+
+def parse_date(df, results_path):
+    """A function to parse the Departure Date column and create two new columns: Departure Month and Departure Year for
+    analysis. Entries in the Departure Date column appear as: 'MMM DD, YYYY'
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe from which to parse the departure date
+    results_path : str
+        The path to the file to which to add the columns
+
+    :returns None
+    """
+
+    print('Parsing flight departure date. Please wait.')
+    start = time.time()
+
+    df_copy = df.copy()
+
+    # Create empty columns for the departure month and year to fill from the parsed departure date column
+    df_copy['Departure Month'] = ''
+    df_copy['Departure Year'] = ''
+
+    for i, row in df_copy.iterrows():
+        date = df_copy.at[i, 'Departure Date']
+        first_space = date.find(' ')  # Get the index of the first space
+        second_space = date.find(' ', first_space + 1, len(date))  # Get the index of the second space
+
+        df_copy.at[i, 'Departure Month'] = date[:first_space]
+        df_copy.at[i, 'Departure Year'] = date[second_space + 1:]
+
+    df_copy.to_csv(results_path, index=False)
+    print('Finished in ' + str((time.time() - start) / 60) + ' mins. Please find the results in ' + results_path + '.')
+
+
+def delete_columns(df, columns, results_path):
+    """A function to delete a given column from the data.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe in which to delete a given column
+    columns : list
+        The list of columns to delete
+    results_path : str
+        The path to file in which to put the remaining data
+
+    :returns None
+    """
+
+    print('Deleting the following columns from the data. Please wait. \n' + columns)
+    start = time.time()
+
+    df_copy = df.copy()
+    df_copy.drop(columns, axis=1)
+    df_copy.to_csv(results_path, index=False)
+
+    print('Finished in ' + str((time.time() - start) / 60) + ' mins. Please find the remaining data in '
           + results_path + '.')
